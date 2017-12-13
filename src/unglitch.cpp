6d9bb986a4562fc58bd01b0d4fd60e6c0e6cd2ad
Don Cross <cosinekitty@gmail.com>
2017-12-13 14:50:13 -0500

    Added debugging support to build.

--- src/unglitch.cpp
+++ src/unglitch.cpp
@@ -4,11 +4,30 @@
     Removes certain glitches that appear when I record radio programs.
 */
 
+#include <iostream>
 #include "tinyxml2.h"
 
 int main(int argc, const char *argv[])
 {
+    using namespace std;
+    using namespace tinyxml2;
+
     int rc = 1;
+
+    if (argc != 2)
+    {
+        cerr << "USAGE: unglitch infile.aup" << endl;
+        return 1;
+    }
+
+    const char *inAudacityProjectFileName = argv[1];
+    XMLDocument doc;
+    XMLError status = doc.LoadFile(inAudacityProjectFileName);
+    if (status != XML_SUCCESS)
+    {
+        cerr << "ERROR " << status << " trying to open Audacity project file '" << inAudacityProjectFileName << "'" << endl;
+        return 1;
+    }
 
     return rc;
 }
