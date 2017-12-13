c5db864237e078d93540671f74bb2928496f33cb
Don Cross <cosinekitty@gmail.com>
2017-12-13 14:12:47 -0500

    Building stub project.

---
+++ src/unglitch.cpp
@@ -0,0 +1,14 @@
+/*
+    unglitch.cpp  -  Don Cross  -  13 December 2017.
+
+    Removes certain glitches that appear when I record radio programs.
+*/
+
+#include "tinyxml2.h"
+
+int main(int argc, const char *argv[])
+{
+    int rc = 1;
+
+    return rc;
+}
